003ca2d7a79aa1ca80dc6318ae4d27fc5159ca51
kei410 <girigirieye@yahoo.co.jp>
2021-09-25 13:26:14 +0900

    コミット

--- www/html/date.php
+++ www/html/date.php
@@ -19,18 +19,3 @@ $w = Week()[date("w")];
 
 echo date("Y/m/d($w)");
 ?>
-
-.container cf {
-    display:flex;
-}
-
-<input id="name" required>
-<input id="name" required>
-required
-
-<input id="zipcode" pattern="\d{3}-?\d{4}" required>
-
-"\d{2,4}-\d{3,4}-\d{3,4}"
-<input id="age" pattern="" required>
-
-pattern="[\d\-]*" maxlength="11">
